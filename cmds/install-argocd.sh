34af8beac69e750aef8c6349c04faebe707f6f89
managekrang <managekrang@163.com>
2023-07-04 14:37:39 +0800

    Update install-argocd.sh

--- cmds/install-argocd.sh
+++ cmds/install-argocd.sh
@@ -1,3 +1,3 @@
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/v2.5.2/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.5.2/manifests/install.yaml
 kubectl patch service argocd-server -n argocd -p '{"metadata":{"annotations":{"traefik.ingress.kubernetes.io/service.serverstransport":"traefik-default@kubernetescrd"}}}'
